a98288607995e7c17602c1a414d9518ca31dcc4d
ahmadn7 <gahmadn7@gmail.com>
2018-03-14 17:19:06 -0400

    hi guys

--- src/com/FirstTry.java
+++ src/com/FirstTry.java
@@ -3,7 +3,7 @@ package com;
 public class FirstTry {
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("Group practice");
+		System.out.println("Group practice a");
 	}
 
 }
