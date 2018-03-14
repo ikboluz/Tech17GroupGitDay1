8b2404ddc7f5d45ee419fd577eafe44e7824961b
ahmadn7 <gahmadn7@gmail.com>
2018-03-14 17:28:55 -0400

    hey

--- src/com/FirstTry.java
+++ src/com/FirstTry.java
@@ -3,12 +3,14 @@ package com;
 public class FirstTry {
 	public static void main(String[] args) {
 		System.out.println("Hello");
-
+		System.out.println("Group work");
 		System.out.println("Group practice ");
 
 		System.out.println("what time is the meeting?");
-		System.out.println("Group work");
+		
 
+		System.out.println("we need to work on project!");
+		
 		
 		System.out.println("Hey Ikbol");
 
